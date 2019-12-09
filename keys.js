a9caf2cf51ffc4add0fe4b73ec44c02e4f50993d
Muey Surigao <muey.surigao@gmail.com>
2019-12-08 16:08:59 -0800

    made gitignore page

---
+++ keys.js
@@ -0,0 +1,18 @@
+// console.log('Spotify API is loaded. ');
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };
+  
+  // console.log('OMDB API is loaded. ');
+  
+  exports.omdb = {
+    id: process.env.OMDB_ID,
+  };
+  
+  // console.log('Bands In Town API is loaded. ');
+  
+  exports.bandsInTown = {
+    id: process.env.BANDS_IN_TOWN_ID,
+  };
